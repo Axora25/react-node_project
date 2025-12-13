910587bb1164b26c88a2dd3d5b4287660f5b65f9
KuldipRana03 <149886860+KuldipRana03@users.noreply.github.com>
2025-12-13 13:36:03 +0530

    Add files via upload

---
+++ frontend/src/pages/Support.jsx
@@ -0,0 +1,11 @@
+import SupportForm from "../components/SupportForm";
+
+const Support = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <SupportForm />
+    </div>
+  );
+};
+
+export default Support;
